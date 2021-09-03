28b3c86dda0e562075fac559c1f24c1e62da6c74
Aryetis <aramirg@hotmail.fr>
2021-09-04 01:16:55 +0200

    updated libkobo.so to match the src, commenting more useful variables from descriptors

--- screensettings.cpp
+++ screensettings.cpp
@@ -2,6 +2,7 @@
 #include "ui_screensettings.h"
 #include "koboplatformfunctions.h"
 
+
 ScreenSettings::ScreenSettings(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ScreenSettings),
@@ -23,12 +24,23 @@ ScreenSettings::~ScreenSettings()
 void ScreenSettings::on_LuminositySlider_valueChanged(int value)
 {
     luminosity = value;
+    KoboDeviceDescriptor desc = KoboPlatformFunctions::getKoboDeviceDescriptor();
+    // TODO use those
+    //    desc.frontlightSettings.frontlightMin
+    //    desc.frontlightSettings.frontlightMax
+    //    desc.frontlightSettings.hasFrontLight
+    //    desc.frontlightSettings.naturalLightMax
+    //    desc.frontlightSettings.naturalLightMin
+    //    desc.frontlightSettings.naturalLightInverted
+    //    desc.frontlightSettings.hasNaturalLightMixer
+    //    desc.frontlightSettings.hasNaturalLight
     KoboPlatformFunctions::setFrontlightLevel(luminosity, tint);
 }
 
 void ScreenSettings::on_TintSlider_valueChanged(int value)
 {
     tint = value/10;
+    KoboDeviceDescriptor desc = KoboPlatformFunctions::getKoboDeviceDescriptor();
     KoboPlatformFunctions::setFrontlightLevel(luminosity, tint);
 }
 
