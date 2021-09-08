86860ca43d506da89920b932b73dd78527938942
Aryetis <aramirg@hotmail.fr>
2021-09-08 12:01:51 +0200

    fix screensettings serialization

--- screensettings.cpp
+++ screensettings.cpp
@@ -21,7 +21,7 @@ ScreenSettings::ScreenSettings(QWidget *parent) :
         noSettingAvailable = false;
     }
     else
-        ui->LuminositySlider->setVisible(false);
+        ui->LuminosityContainer->setEnabled(false);
 
     if (desc.frontlightSettings.hasNaturalLight)
     {
@@ -30,11 +30,11 @@ ScreenSettings::ScreenSettings(QWidget *parent) :
         noSettingAvailable = false;
     }
     else
-        ui->TintSlider->setVisible(false);
+        ui->TintContainer->setEnabled(false);
 
     if (!noSettingAvailable)
     {
-        ui->NoSettingsAvailable->setVisible(false);
+        ui->NoSettingsContainer->setEnabled(false);
         KoboPlatformFunctions::setFrontlightLevel(static_cast<int>(luminosity), static_cast<int>(tint));
     }
 
@@ -51,7 +51,7 @@ void ScreenSettings::on_LuminositySlider_valueChanged(int value)
     luminosity = ((static_cast<float>(desc.frontlightSettings.frontlightMax - desc.frontlightSettings.frontlightMin)
                    / static_cast<float>(ui->LuminositySlider->maximum()) * static_cast<float>(value)))
                 + static_cast<float>(desc.frontlightSettings.frontlightMin);
-    settingsSerializer->setValue("ScreenSettings/luminosity", luminosity);
+    settingsSerializer->setValue("ScreenSettings/luminosity", value);
     KoboPlatformFunctions::setFrontlightLevel(static_cast<int>(luminosity), static_cast<int>(tint));
 }
 
@@ -60,6 +60,6 @@ void ScreenSettings::on_TintSlider_valueChanged(int value)
     tint =  ((static_cast<float>(desc.frontlightSettings.naturalLightMax - desc.frontlightSettings.naturalLightMin)
               / static_cast<float>(ui->TintSlider->maximum()) * static_cast<float>(value)))
            + static_cast<float>(desc.frontlightSettings.naturalLightMin);
-    settingsSerializer->setValue("ScreenSettings/tint", tint);
+    settingsSerializer->setValue("ScreenSettings/tint", value);
     KoboPlatformFunctions::setFrontlightLevel(static_cast<int>(luminosity), static_cast<int>(tint));
 }
