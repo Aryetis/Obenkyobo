1059c5e989500700904140e6577d3047f61c37db
Aryetis <aramirg@hotmail.fr>
2021-10-08 02:09:03 +0200

    more QScrollbar customization

--- symbolsetting.cpp
+++ symbolsetting.cpp
@@ -4,6 +4,8 @@
 #include "symbolsettingsection.h"
 #include "symbolstables.h"
 
+#include <QScrollBar>
+
 #define SCROLLBAR_WIDTH 40
 
 SymbolSetting::SymbolSetting(QWidget *parent) :
@@ -14,6 +16,9 @@ SymbolSetting::SymbolSetting(QWidget *parent) :
 
     SymbolsTables::HiraganaSymbolsTableFamily.InitializeSerializedVals();
     SymbolsTables::KatakanaSymbolsTableFamily.InitializeSerializedVals();
+
+    ui->scrollArea->horizontalScrollBar()->setPageStep(50); // TODO now fix button step value slooooooow
+    ui->scrollArea->verticalScrollBar()->setPageStep(10);
 
     GetMy::Instance().SetSymbolSettingWidget(this);
 }
@@ -26,51 +31,42 @@ SymbolSetting::~SymbolSetting()
 void SymbolSetting::resizeEvent(QResizeEvent* event)
 {
    QWidget::resizeEvent(event);
-   setStyleSheet(QString("QScrollBar:vertical { width : %1px }").arg(SCROLLBAR_WIDTH));
-//   setStyleSheet(QString("QScrollBar::handle:vertical { background-color: red; }"));
+//   setStyleSheet(QString("QScrollBar:vertical { width : %1px }").arg(SCROLLBAR_WIDTH));
 
+   // TODO now fix image not showing up for button
    setStyleSheet(
-
-               "QScrollBar {                                                    "
-               " border: 2px solid grey;                                              "
-               " background: white;                                                 "
-               " width: 40px;                                                         "
-               " margin: 42px 0 42px 0;                                               "
-               "}                                                                        "
-               "QScrollBar::handle {                                            "
-               " background: black;                                                   "
-               " min-height: 20px;                                                    "
-               "}                                                                        "
-               "QScrollBar::add-line {                                          "
-               " border: 2px solid grey;                                              "
-               " background: white;                                                 "
-               " height: 40px;                                                        "
-               " subcontrol-position: bottom;                                         "
-               " subcontrol-origin: margin;                                           "
-               "}                                                                        "
-               "																	  "
-               "QScrollBar::sub-line {                                          "
-               " border: 2px solid grey;                                              "
-               " background: white;                                                 "
-               " height: 40px;                                                        "
-               " subcontrol-position: top;                                            "
-               " subcontrol-origin: margin;                                           "
-               "}                                                                        "
-               "QScrollBar::up-arrow, QScrollBar::down-arrow, "
-               "QScrollBar:left-arrow, QScrollBar::right-arrow {         "
-               " border: 2px solid grey;                                              "
-               " width: 3px;                                                          "
-               " height: 3px;                                                         "
-               " background: white;                                                   "
-               "}                                                                        "
-               "																	  "
-               "QScrollBar::add-page, QScrollBar::sub-page {           "
-               " background: none;                                                    "
-               "}             "
-
-
-
-
+"QScrollBar                                                                                                                 "
+"{ border: 2px solid grey; background: white; width: 40px; margin: 42px 0 42px 0; }                                         "
+"QScrollBar::handle                                                                                                         "
+"{ background: rgb(60,60,60); min-height: 20px; }                                                                                   "
+"QScrollBar::add-line                                                                                                       "
+"{ border: 2px solid grey; background: white; height: 40px; subcontrol-position: bottom; subcontrol-origin: margin; }       "
+"QScrollBar::sub-line                                                                                                       "
+"{ border: 2px solid grey; background: white; height: 40px; subcontrol-position: top; subcontrol-origin: margin; }          "
+"QScrollBar::add-page, QScrollBar::sub-page                                                                                 "
+"{ background: none; }                                                                                                      "
+"                                                                                                                           "
+"QScrollBar::up-arrow                                                                                                       "
+"{                                                                                                                          "
+"	image: url(:/pictures/upArrow.png);                                                                              "
+"	border: 2px solid grey; width: 3px; height: 3px; background: none;                                                     "
+"}                                                                                                                          "
+"QScrollBar::down-arrow                                                                                                     "
+"{                                                                                                                          "
+"	image: url(:/pictures/downArrow.png);                                                                            "
+"	border: 2px solid grey; width: 3px; height: 3px; background: none;                                                     "
+"}                                                                                                                          "
+"                                                                                                                           "
+"QScrollBar:left-arrow                                                                                                      "
+"{                                                                                                                          "
+"	image: url(:/pictures/leftArrow.png);                                                                            "
+"	border: 2px solid grey; width: 3px; height: 3px; background: none;                                                     "
+"}                                                                                                                          "
+"QScrollBar::right-arrow                                                                                                    "
+"{                                                                                                                          "
+"	image: url(:/pictures/rightArrow);                                                                               "
+"	border: 2px solid grey; width: 3px; height: 3px; background: none;                                                     "
+"}                                                                                                                          "
 
 );
 
