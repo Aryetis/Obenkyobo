00ca298d8f6464bc40cbc975c569c4c5b3064cef
Aryetis <aramirg@hotmail.fr>
2021-10-11 15:11:27 +0200

    getting lost with lib stuff

--- OtherFiles/Resources/debugEnv.sh
+++ OtherFiles/Resources/debugEnv.sh
@@ -1,6 +1,8 @@
 export APPNAME=Obenkyobo
 export ADDSPATH=/mnt/onboard/.adds
 export APPPATH=${ADDSPATH}/${APPNAME}
+export QTPATH=${ADDSPATH}/qt-linux-5.15.2-kobo
+export QTPLUGINOBENKYOBO=kobo
 
 LOGFILE=${APPPATH}/log.txt
 
@@ -8,4 +10,5 @@ LOGFILE=${APPPATH}/log.txt
 source ${APPPATH}/exit_nickel.sh
 
 # export QT stuff
-export QT_QPA_PLATFORM=kobo:touchscreen_rotate=auto:touchscreen_invert_x=auto:touchscreen_invert_y=auto:logicaldpitarget=108
+export LD_LIBRARY_PATH=${QTPATH}/lib:lib:
+export QT_QPA_PLATFORM=${QTPLUGINOBENKYOBO}:touchscreen_rotate=auto:touchscreen_invert_x=auto:touchscreen_invert_y=auto:logicaldpitarget=108
