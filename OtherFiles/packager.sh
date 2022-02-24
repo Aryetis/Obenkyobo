69621dd5ae8d3a4024e8e1c27410a53be385c10e
aryetis <aramirg@hotmail.fr>
2022-02-24 18:21:42 +0100

    removing ugly fonts, removing outdated libkobo.so, updating Obenkyobo version in packager.sh

--- OtherFiles/packager.sh
+++ OtherFiles/packager.sh
@@ -9,7 +9,7 @@ LongName="Obenkyobo !"
 RemoteIp="192.168.1.18"
 Author="Aryetis"
 KfMonDbComment="Obenkyo clone-ish"
-Version="0.1.2"
+Version="0.2.0"
 LauncherName=$4_launcher.sh
 QtPluginKobo=kobo_obenkyobo
 
